f0ba609b0ce3592605aafda5de6679434aa034f3
Yuki Okamura <impossible.not.i.m.possible@gmail.com>
2016-09-04 05:49:02 +0900

    base moving water strider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function(){
-	var RANGE = 50,
+	var RANGE = 30,
 		N = 20;
 	var width = window.innerWidth,
 		height = window.innerHeight,
@@ -11,14 +11,31 @@
 		this.x = x;
 		this.y = y;
 		this.size = 5;
-		this.step = Math.random()*5;
+		this.step = Math.random()*50;
 	}
 	WaterStrider.prototype.update = function(timestamp) {
-		
+		++this.step;
+		if (this.step > 50) {
+			this.step = 0;
+			var nx = this.x + RANGE*2*Math.random()-RANGE,
+				ny = this.y + RANGE*2*Math.random()-RANGE;
+			this.x = nx;
+			this.y = ny;
+			draw(nx,ny,this.size);
+		}else{
+			draw(this.x,this.y,this.size);
+		}
+
 	};
 
+	function draw(x,y,size){
+		context.beginPath();
+		context.arc(x,y,size,0,Math.PI*2,false);
+		context.fillStyle = "#FFF";
+		context.fill();
+	}
+
 	function init(){
-		console.log("start demo");
 		canvas = document.getElementById( "canvas" );
 		context = canvas.getContext( "2d" );
 		canvas.width = width;
@@ -38,6 +55,9 @@
 		_frame = requestAnimationFrame(onFrame);
 		++timestamp;
 		context.clearRect(0,0,width,height);
-
+		for (var i = 0; i < water_striders.length; i++) {
+			water_striders[i].update();
+		}
 	}
+	init();
 })();
